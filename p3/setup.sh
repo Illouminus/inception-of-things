278742733b1ec2b5c52333c8c86e0ac36570f1c7
Adrien Hors <ahors@f6r7s15.clusters.42paris.fr>
2025-10-20 12:41:36 +0200

    p3 - new set-up file

--- p3/setup.sh
+++ p3/setup.sh
@@ -4,16 +4,37 @@
 set -e
 
 # Updating packages
-echo "=== Mise à jour des dépôts ==="
+echo "=== Updating packages ==="
 sudo apt update
 sudo apt upgrade
 
 # Installing required packages
 
-echo "=== Installation de Docker ==="
+echo "=== STEP 1 ==="
+echo "Docker config"
+sudo usermod -aG docker $(whoami)
+
+echo "=== STEP 2 ==="
+
+echo "Installing kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+echo "Validate binary"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+
+echo "Check file integrity"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+echo "Make kubectl executable and accessible"
+chmod +x kubectl &&  mv ./kubectl /usr/local/bin
 
 
-echo "=== Installation de kubectl ==="
+echo "=== STEP 3 ==="
+echo "Installing k3d"
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
 
-echo "=== Installation de k3d ==="
+echo "Installing the auto-completion scripts for k3d"
+echo "source <(k3d completion bash)" >> ~/.bashrc
+
+echo "..::OK::.."
+
